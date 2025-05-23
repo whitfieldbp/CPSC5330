e7dd8c8f1f43fb9d50017f6a634aea4e2732bf63
Benjamin Whitfield <benwhitfield@Benjamins-MacBook-Air.local>
2025-05-22 19:13:58 -0500

    Simple BMX Logo

---
+++ module1/module1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  module1
+//
+//  Created by Benjamin Whitfield on 5/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
