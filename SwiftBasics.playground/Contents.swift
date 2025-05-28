aca25805944ebfafe21e06a014ab5eeebbff3b17
Benjamin Whitfield <benwhitfield@Benjamins-MacBook-Air.local>
2025-05-27 21:51:51 -0500

    Added all functions.

--- SwiftBasics.playground/Contents.swift
+++ SwiftBasics.playground/Contents.swift
@@ -2,15 +2,10 @@ import UIKit
 
 // Movies based on Video Games with their Metacritic scores.
 
-func findMinMax(scores: [Int]) -> (min: Int, max: Int, avg: Int) {
-    let minScore = scores.min() ?? 0
-    let maxScore = scores.max() ?? 0
-    let avgScore = scores.reduce(0, +) / scores.count
-    return (minScore, maxScore, avgScore)
-}
-
+// Tuple setup
 typealias MovieGame = (name: String, score: Int)
 
+//Array setup
 let movie : [MovieGame] = [
     ("Warcraft", 32),
     ("Resident Evil", 35),
@@ -21,10 +16,27 @@ let movie : [MovieGame] = [
     ("The Super Mario Bros. Movie", 46)
 ]
 
-print(movie[0].name)
-print(movie[3].score)
+// Summary function
+func summary(_ movie: [MovieGame]) {
+    for (name, score) in movie {
+        print("\(name): \(score)")
+    }
+}
+summary(movie)
+
+// Function with min, max, and avg score
+func findMinMax(scores: [Int]) -> (min: Int, max: Int, avg: Int) {
+    let minScore = scores.min() ?? 0
+    let maxScore = scores.max() ?? 0
+    let avgScore = scores.reduce(0, +) / scores.count
+    return (minScore, maxScore, avgScore)
+}
 
 let scoreResult = findMinMax(scores: movie.map(\.score))
 print("Min score: \(scoreResult.min)")
 print("Max score: \(scoreResult.max)")
 print("Average score: \(scoreResult.avg)")
+
+// Filter function with closure for score above 50.
+let highScore = movie.filter { $0.score >= 50 }
+print("High Scores:", highScore)
