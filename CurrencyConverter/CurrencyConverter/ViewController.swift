ee9ec5fb18d1148c30ad570be4f94281528f44ba
Benjamin Whitfield <benwhitfield@Benjamins-MacBook-Air.local>
2025-06-28 18:27:46 -0500

    Created File

---
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Benjamin Whitfield on 6/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
