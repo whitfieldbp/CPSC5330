1c9bf2e4e48a93bd4037edf99f681472c6c6ee2a
Benjamin Whitfield <benwhitfield@Benjamins-MacBook-Air.local>
2025-06-29 17:19:30 -0500

    Design seems to be finished. Working on logic

---
+++ CurrencyConverter/CurrencyConverter/Model/ConverterLogic.swift
@@ -0,0 +1,25 @@
+//
+//  ConverterLogic.swift
+//  CurrencyConverter
+//
+//  Created by Benjamin Whitfield on 6/29/25.
+//
+
+import Foundation
+
+struct ConverterLogic {
+    
+    var usdAmount: Int = 0;
+    
+    var isEuroSelected = false
+    var isPesoSelected = false
+    var isFlorinSelected = false
+    var isRupeeSelected = false
+    
+    let euroRate = 0.85
+    let pesoRate = 18.82
+    let florinRate = 1.79
+    let rupeeRate = 85.49
+    
+    
+}
